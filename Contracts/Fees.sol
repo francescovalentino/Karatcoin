51e206bcf4c24db488dead5f882641a178a5731d
Michal Mikolajczyk <30987326+mikolajczykx@users.noreply.github.com>
2017-08-14 18:13:20 +0200

    Update Fees.sol

--- Contracts/Fees.sol
+++ Contracts/Fees.sol
@@ -3,9 +3,18 @@ import "./Token.sol";
 import "./Settings.sol";
 
 contract Collector {
-  //...
+  address public owner;
+  address public FeesPool;
+  address public DAO_Tokens;
+  uint256 public payout_Time;
 }
 
 contract GoldFees {
-  //...
+  address public DAO_Tokens;
+  address public GOLD_Tokens;
+  bytes32 public environment;
+  uint256 public timeLength;
+  uint256 public timeCount;
+  uint256 public collectDuration;
+  mapping (uint256 => Time) times;
 }
