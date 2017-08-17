b25327f58fe88c79de540d9a7bc30bb96e337f64
Chris Wagner <31094893+chriswagner81@users.noreply.github.com>
2017-08-17 11:20:52 +0200

    Update Dao.sol

--- Contracts/Dao.sol
+++ Contracts/Dao.sol
@@ -2,8 +2,9 @@ pragma solidity ^0.4.0;
 
 import "./Settings.sol";
 
-contract Certificate  {  
+contract Certificate {  
   address public owner;
+  bool public locked;
   uint256 public totalSupply;
   mapping (address => uint256) balances;
   mapping (address => mapping (address => uint256)) allowed;
@@ -23,37 +24,39 @@ contract Certificate  {
     owner = msg.sender;
   }
 
+  function safeToAdd(uint a, uint b) returns (bool) {
+    return (a + b >= a);
+  }
+
+  function safeToSubtract(uint a, uint b) returns (bool) {
+    return (b <= a);
+  }
+
+  function addSafely(uint a, uint b) returns (uint result) {
+  }
+
+  function subtractSafely(uint a, uint b) returns (uint) {
+  }
+
   function balanceOf(address _owner) constant returns (uint256 balance) {
     return balances[_owner];
   }
 
   function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-    if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-      //...
-      return true;
-    } else {
-      return false;
-    }
   }
 
   function transfer(address _to, uint256 _value) returns (bool success) {
-    if (balances[msg.sender] >= _value && _value > 0) {
-      //...
-      success = true;
-    } else {
-      success = false;
-    }
-    return success;
   }
 
   function approve(address _spender, uint256 _value) returns (bool success) {
-    //...
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
     success = true;
     return success;
   }
 
   function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-    //...
+    remaining = allowed[_owner][_spender];
     return remaining;
   }
 
@@ -70,28 +73,35 @@ contract Token {
   address public config;
   address public certificateLedger;
   address public dao;  
+  bool public locked;
   uint256 public totalSupply;
   mapping (address => uint256) balances;
   mapping (address => bool) seller;
   mapping (address => mapping (address => uint256)) allowed;
 
   modifier ifOwner() {
-    if (msg.sender != owner) throw;
+    if (msg.sender != owner) 
+    throw;
     _;
   }  
   
   modifier ifDao() {
-    if (msg.sender != dao) throw;
+    if (msg.sender != dao) 
+    throw;
     _;
   }
 
   modifier ifSales() {
-    if (!seller[msg.sender]) throw; 
+    if (!seller[msg.sender]) 
+    throw; 
     _; 
   }
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
   event Approval(address indexed _owner, address indexed _spender, uint256  _value);
+
+  function Token(address _config) {
+  }
 
   function safeToAdd(uint a, uint b) returns (bool) {
     return (a + b >= a);
@@ -102,17 +112,9 @@ contract Token {
   }
 
   function addSafely(uint a, uint b) returns (uint result) {
-    if (!safeToAdd(a, b)) {
-      throw;
-    } else {
-      result = a + b;
-      return result;
-    }
   }
 
   function subtractSafely(uint a, uint b) returns (uint) {
-    if (!safeToSubtract(a, b)) throw;
-    return a - b;
   }
 
   function balanceOf(address _owner) constant returns (uint256 balance) {
@@ -120,26 +122,14 @@ contract Token {
   }
 
   function transfer(address _to, uint256 _value) returns (bool success) {
-    if (balances[msg.sender] >= _value && _value > 0) {
-      //...
-      success = true;
-    } else {
-      success = false;
-    }
-    return success;
   }
 
   function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-    if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-      //...
-      return true;
-    } else {
-      return false;
-    }
   }
 
   function approve(address _spender, uint256 _value) returns (bool success) {
-    //...
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
     success = true;
     return success;
   }
@@ -164,7 +154,10 @@ contract Token {
   }
 
   function registerDao(address _dao) ifOwner returns (bool success) {
+    if (locked == true) 
+    return false;
     dao = _dao;
+    locked = true;
     return true;
   }
 
@@ -174,7 +167,7 @@ contract Token {
   }
 
   function setOwner(address _newowner) ifDao returns (bool success) {
-    if(Certificate(certificateLedger).setOwner(_newowner)) {
+    if (Certificate(certificateLedger).setOwner(_newowner)) {
       owner = _newowner;
       success = true;
     } else {
